4dc632113e2c88ce29fc3015ab88773cf16005f2
eerok95 <eero.e.keinanen@gmail.com>
2025-10-24 15:38:13 +0300

    added content behind links, made a recursive generate_pages function

---
+++ src/generate_pages_recursive.py
@@ -0,0 +1,19 @@
+import os
+from generate_page import generate_page
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    contents = os.listdir(dir_path_content)
+    for content in contents:
+        content_path = os.path.join(dir_path_content, content)
+        content_dest_path = os.path.join(dest_dir_path, content)
+        print("-----------------------")
+        print(f"content_dest_path: {content_dest_path}")
+        print("-----------------------")
+        if os.path.isfile(content_path) and os.path.splitext(content_path)[1] == ".md":
+            content_dest_path = content_dest_path[:-3] + ".html"
+            generate_page(content_path, template_path, content_dest_path)
+            continue
+        if os.path.isdir(content_path):
+            if not os.path.exists(content_dest_path):
+                os.mkdir(content_dest_path)
+                print(f"copied directory: {content_path} to {content_dest_path}")
+            generate_pages_recursive(content_path, template_path, content_dest_path)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 
 from copy_contents_from_source import copy_contents
-from generate_page import generate_page
+from generate_pages_recursive import generate_pages_recursive
 def main():
     copy_contents("./static", "./public")
-    generate_page("./content/index.md","template.html", "./public/index.html")
+    generate_pages_recursive("./content","template.html", "./public")
 main()
